af7df61de623cab88341c53028d28d861718aa08
Daniel Baumann <dbaumann84@gmail.com>
2023-09-13 21:24:57 -0300

    inicio frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./router');
+
 
 
 const app = express();
 
 app.use(express.json());
+//libera acesso para qualquer host
+app.use(cors());
 app.use(router);
 
 
